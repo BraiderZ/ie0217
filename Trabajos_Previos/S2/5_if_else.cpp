144f9d882ae5c5ebd21dd3368b7aef8e6e74d1b2
BraiderZ <luisjbc2112@gmail.com>
2024-01-03 19:21:15 -0600

    Quinto y sexto archivo
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

---
+++ Trabajos_Previos/S2/5_if_else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number = 0;
+
+    //Iniciamos condición. Si se cumple, se realiza lo que dica el "if", sino, lo que diga el "else"
+
+    if (number > 0) { // Si colocamos un <, cambiamos la condición a lo contrario
+       cout << "Se cumple el if"; 
+    }
+    else {
+        cout << "No se cumple el if"; 
+    }
+    
+    return 0;
+}
