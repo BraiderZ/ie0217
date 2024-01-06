db4cb0743a547f7b0491a97d7a69300163d3900a
BraiderZ <luisjbc2112@gmail.com>
2024-01-06 15:43:44 -0600

    Se eliminaron los archivos extra. Ya se habían guardado estos archivos en la nueva carpeta para separar previos
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- Trabajos_Previos/S3/1_Encabezados_Sum.cpp
+++
@@ -1,6 +0,0 @@
-#include "1_Encabezados_Sum.hpp"
-
-//Agregamos lo que hace la función
-int sum(int a, int b) {
-    return (a + b);
-}
--- Trabajos_Previos/S3/1_Encabezados_Sum.hpp
+++
@@ -1,8 +0,0 @@
-//Si no existe la variable, la define
-#ifndef SUM_H
-#define SUM_H
-
-//Prototipo de la fucnión
-int sum(int a, int b);
-
-#endif
