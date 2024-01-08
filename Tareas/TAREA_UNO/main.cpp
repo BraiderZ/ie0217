9bedcc7d55cb5f6261cb8b943de720d3b913214e
BraiderZ <luisjbc2112@gmail.com>
2024-01-08 15:19:17 -0600

    Documentación del main realizada
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- Tareas/TAREA_UNO/main.cpp
+++ Tareas/TAREA_UNO/main.cpp
@@ -3,6 +3,25 @@
 #include <iostream>
 #include <random>
 
+/**
+ * @file main.cpp
+ * 
+ * @brief Se encarga de la inicialización del juego. Invoca y envía los parámetros iniciales al resto del programa
+ * 
+ * @var nivel_dificultad
+ * Flag encargada de determinar una de las dos dificultades del juego (dificultad fácil por defecto).  
+ * @var cantidad_intentos
+ * Cantidad de intentos para adivinar el número secreto. (4 por defecto)
+ * @var intervalo_valores
+ * Arreglo que posee el número menor y mayor definido por el usuario (0-20 por defecto). 
+ * @var ajuste_dificultad_dificil
+ * Valor necesario para definir el intervalo de valores que irán dentro de las diferentes pistas en la dificultad dificil (3 por defecto). 
+ * @var numero_secreto
+ * Número que deberá de avriguar el usuario en cada partida. 
+ * 
+ * @return int 
+ */
+
 int main(){
     int nivel_dificultad = 1;
     int cantidad_intentos = 4;
