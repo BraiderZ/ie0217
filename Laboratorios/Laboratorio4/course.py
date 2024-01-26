2e71889822c80995f1eab43b19fabe851f150711
BraiderZ <luisjbc2112@gmail.com>
2024-01-25 18:03:55 -0600

    Documentación de la clase Course
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- Laboratorios/Laboratorio4/course.py
+++ Laboratorios/Laboratorio4/course.py
@@ -1,8 +1,10 @@
+# Se crea la clase para agrupar los cursos
 class Course():
     def __init__(self, course_code, course_name):
         self.course_code = course_code
         self.course_name = course_name
 
+    #Imprimir la información
     def display_info(self):
         print(f"Course code: {self.course_code}\
               \nCourse name: {self.course_name}\n")
