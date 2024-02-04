dcddcf205dc84d566f9510a7a5040652debbb3eb
BraiderZ <luisjbc2112@gmail.com>
2024-02-04 16:21:56 -0600

    Existe el main que se encargará de unificar todo el código.
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- Tareas/TAREA_CINCO/Practica1_CrearDataFrame.py
+++ Tareas/TAREA_CINCO/Practica1_CrearDataFrame.py
@@ -5,6 +5,3 @@ class DatosCsv:
     def __init__(self, nombre_csv):
         self.datos = pd.read_csv(nombre_csv)
         print(self.datos)
-
-
-datos_csv = DatosCsv("MER_T01_09.csv")
---
+++ Tareas/TAREA_CINCO/Practica1_main.py
@@ -0,0 +1,9 @@
+from Practica1_CrearDataFrame import DatosCsv
+
+
+def main():
+    datos = DatosCsv("MER_T01_09.csv")
+    print(datos)
+
+
+main()
