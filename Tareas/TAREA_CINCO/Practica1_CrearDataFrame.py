956a89203cfa8306bfd6c5e47c00db2bd87880eb
BraiderZ <luisjbc2112@gmail.com>
2024-02-05 06:50:30 -0600

    Se mejoró la manera de procesar los datos y dividirlos en dos grupos: registro de diferentes autos eléctricos y cantidad total de registros y porcentaje de autos eléctricos.
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

--- Tareas/TAREA_CINCO/Practica1_CrearDataFrame.py
+++ Tareas/TAREA_CINCO/Practica1_CrearDataFrame.py
@@ -4,32 +4,57 @@ import pandas as pd
 class DatosCsv:
     def __init__(self, nombre_csv):
         self.datos = pd.read_csv(nombre_csv)
+        self.datos_por_auto = pd.DataFrame()
+        self.datos_totales = pd.DataFrame()
 
     def limpiarDatos(self):
-        self.datos.dropna()
-        
-        columnas_mantener = ['Value', 'YYYYMM', 'Description']
+        self.datos.dropna(inplace=True)
+
+        columnas_mantener = ['Value', 'YYYYMM', 'MSN']
         iterador_columnas = iter(self.datos)
-        
+
         for columna in iterador_columnas:
             if columna not in columnas_mantener:
                 self.datos.drop(columna, axis=1, inplace=True)
-        print(self.datos)
+        
+        self.separar_datos()
+        self.multiplicarValores()
+
+    def separar_datos(self):
+        self.datos_por_auto = self.datos[self.datos["MSN"].isin(["BTVHNUS", "PHVHNUS", "ELVHNUS", "HGVHNUS"])]
+        self.datos_totales = self.datos[self.datos["MSN"].isin(["LDVHNUS", "ELVHSUS"])]
 
     def multiplicarValores(self):
-        for indices, _ in self.datos.iterrows():
-            self.datos.at[indices, 'Value'] *= 1000
+        self.datos_por_auto['Value'] *= 1000
 
     def analisisExhaustivo(self):
-        descripcion = self.datos.describe()
+        descripcion = self.datos_por_auto.describe()
         estadisticas_valores = descripcion['Value']
         cantidad_valores = estadisticas_valores['count']
         promedio_valores = estadisticas_valores['mean']
         valor_minimo = estadisticas_valores['min']
         valor_maximo = estadisticas_valores['max']
-        print(f"Algunos valores descriptivos en los datos analizados son:\
-              \nCantidad de datos totales: {cantidad_valores}\
-              \nPromedio de vehículos totales: {round(promedio_valores)}\
-              \nValor analizado con menos vehículos: {round(valor_minimo)}\
-              \nValor analizado con más vehículos: {round(valor_maximo)}")
+        print(f"Datos sobre vehiculos electricos registrados en EEUU:\
+              \nCantidad de datos relacionados a autos electricos: {cantidad_valores}\
+              \nPromedio de vehículos totales registrados: {round(promedio_valores)}\
+              \nValor analizado con menos registro vehículos: {round(valor_minimo)}\
+              \nValor analizado con más registro vehículos: {round(valor_maximo)}")
 
+    def generador_informe(self, fecha):
+        for _, filas in self.datos_por_auto.iterrows():
+            if (filas.iloc[1] == fecha):
+                yield filas.iloc[2]
+
+    def informe_detallado_fecha(self):
+        fechas = {201213: 0, 201313: 0, 201413: 0, 201513: 0,
+                  201613: 0, 201713: 0, 201813: 0, 201913: 0,
+                  202013: 0, 202113: 0, 202213: 0}
+        for fecha in fechas:
+            for registro in self.generador_informe(fecha):
+                fechas[fecha] += round(registro)
+
+        print("---Cantidad de vehículos electricos registrados fecha---")
+        ano = 2012
+        for fecha in fechas:
+            print(f"{ano}/13: {fechas[fecha]}")
+            ano += 1
--- Tareas/TAREA_CINCO/Practica1_main.py
+++ Tareas/TAREA_CINCO/Practica1_main.py
@@ -5,8 +5,8 @@ def main():
     try:
         datos = DatosCsv("MER_T01_09.csv")
         datos.limpiarDatos()
-        datos.multiplicarValores()
         datos.analisisExhaustivo()
+        datos.informe_detallado_fecha()
     except FileNotFoundError:
         print("Error: el archivo csv enviado para analizar no existe.")
     except Exception as e:
