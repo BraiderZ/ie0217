cacbba8014102217664dc867066725e6435976f5
BraiderZ <luisjbc2112@gmail.com>
2024-02-11 18:09:12 -0600

    Se creó el paquete. Además, existe el archivo llamado request encargado de obtener los datos.
    Signed-off-by: BraiderZ <luisjbc2112@gmail.com>

---
+++ Tareas/TAREA_SEIS/cars_analysis/request.py
@@ -0,0 +1,7 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+
+dataset_name = "akshaydattatraykhare/car-details-dataset"
+api.dataset_download_files(dataset_name, path="./cars_analysis", unzip=True)
